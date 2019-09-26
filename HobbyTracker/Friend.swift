89747845da7a4fb1fce8844037f8ed2f44ed5cba
Craig Swanson <cswanson02@me.com>
2019-09-25 20:50:09 -0500

    Added Friend model to project

---
+++ HobbyTracker/Friend.swift
@@ -0,0 +1,15 @@
+//
+//  Friend.swift
+//  HobbyTracker
+//
+//  Created by Craig Swanson on 9/25/19.
+//  Copyright © 2019 Craig Swanson. All rights reserved.
+//
+
+import Foundation
+
+struct Friend {
+    let name: String
+    let hometown: String
+    var hobbies: [String] = []
+}
